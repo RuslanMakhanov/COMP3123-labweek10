11fcc79b36433317347d3237cc53f3351e421b47
RuslanNnNnNn <95371964+RuslanMakhanov@users.noreply.github.com>
2023-11-12 21:44:22 -0500

    first coommiit

---
+++ labweek10/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import LoginFunction from './LoginFunction';
+
+function App() {
+  return (
+    <>
+      <h1>Week10 - Lab</h1>
+      
+      <LoginFunction/>
+    </>
+  );
+}
+
+export default App;
---
+++ labweek10/src/LoginFunction.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+
+export default function LoginFunction() {
+
+    var [formData, setFormData] = useState({
+        email: '',
+        name: '',
+        address: '',
+        address2: '',
+        city: '',
+        province: '',
+        postalcode: ''
+    })
+    //var [x, setX] = useState(0)
+
+    const provinces = ["Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan"]
+    
+    const onValueChanged = (event) => {
+        setFormData({...formData , [event.target.name]:event.target.value})
+    }
+
+    const onSubmitForm = (event) => {
+        event.preventDefault()
+        console.log("Form Submitted : " +  JSON.stringify(formData))
+    }
+
+  return (
+    <>
+        <h2>Data Entry Form</h2>
+        <form onSubmit={(e) => onSubmitForm(e)} > 
+        <h3>Email</h3>
+            <input 
+                name='email'
+                type="text"
+                onChange={(e) => onValueChanged(e)} 
+                placeholder="Enter Email" />
+        <h3>Name</h3>
+            <input 
+                name='name'
+                type="text"
+                onChange={(e) => onValueChanged(e)} 
+                placeholder="Full Name" />
+        <h3>Address</h3>
+            <input 
+                name='address'
+                type="text"                
+                onChange={(e) => onValueChanged(e)} 
+                placeholder='123 Main St' />
+
+        <h3>Address2</h3>
+             <input 
+                name='address2'
+                type="text"                
+                onChange={(e) => onValueChanged(e)} 
+                placeholder='Aparment, studio,or floor' />
+
+        <h3>City</h3>
+             <input 
+                name='city'
+                type="text"                
+                onChange={(e) => onValueChanged(e)}/>
+            
+        <h3>Province</h3>
+            <select name='province' onChange={(e) => onValueChanged(e)}>
+                {
+                    provinces.map((province) => (
+                            <option key={province} value={province}>{province}</option>
+                    ))
+                }
+            </select>
+        
+            <h3>Postal Code</h3>
+             <input 
+                name='postalcode'
+                type="text"                
+                onChange={(e) => onValueChanged(e)}/>
+            
+            <input 
+                name='btnSubmit'
+                type="submit"
+                value="Login" />
+        </form>
+    </>
+  )
+}
